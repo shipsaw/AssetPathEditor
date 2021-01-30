d83fdf3ec97313f9594bfc47dcc4d55cdda7db11
shipsaw <lyterp@gmail.com>
2021-01-30 14:18:51 -0500

    Added ability to scan multiple files

--- main.go
+++ main.go
@@ -6,74 +6,108 @@ import (
 	"log"
 	"os"
 	"os/exec"
+	"path/filepath"
+	"sort"
+	"strings"
 )
 
-const testFile string = "+000000+000000"
-
-var serzExe = exec.Cmd{
-	Path:   "./files/serz.exe",
-	Args:   []string{"./serz.exe", "./files/" + testFile + ".bin"},
-	Stdout: os.Stdout,
-}
+var file string
 
 type RecordSet struct {
 	XMLName xml.Name `xml:"cRecordSet"`
-	Records []Record `xml:"Record"`
+	Records Record   `xml:"Record"`
 }
 
 type Record struct {
-	XMLName  xml.Name `xml:"Record"`
 	Entities []Entity `xml:"cDynamicEntity"`
 }
 
 type Entity struct {
-	XMLName    xml.Name      `xml:"cDynamicEntity"`
-	Blueprints []BlueprintID `xml:"BlueprintID"`
+	Blueprints BlueprintID `xml:"BlueprintID"`
 }
 
 type BlueprintID struct {
-	XMLName      xml.Name       `xml:"BlueprintID"`
-	BlueprintLib []AbsBluePrint `xml:"iBlueprintLibrary-cAbsoluteBlueprintID"`
+	BlueprintLib AbsBluePrint `xml:"iBlueprintLibrary-cAbsoluteBlueprintID"`
 }
 
 type AbsBluePrint struct {
-	XMLName     xml.Name `xml:"iBlueprintLibrary-cAbsoluteBlueprintID"`
-	BlueprintID string   `xml:"BlueprintID"`
+	BlueprintID string `xml:"BlueprintID"`
 }
 
 func main() {
+	assetMap := make(map[string]int)
+	err := filepath.Walk("./filesBin", func(path string, info os.FileInfo, err error) error {
+		if info.IsDir() != true && filepath.Ext(path) == ".bin" {
+			getFileAssets(path, assetMap)
+		}
+		return nil
+	})
+	// Move xml file
+	err = filepath.Walk("./filesBin", func(path string, info os.FileInfo, err error) error {
+		if info.IsDir() != true && filepath.Ext(path) == ".xml" {
+			splitPath := strings.SplitAfter(path, "\\")
+			newPath := "filesXml\\" + splitPath[1]
+			err = os.Rename(path, newPath)
+			if err != nil {
+				panic(err)
+			}
+		}
+		return nil
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	assetList := make([]string, len(assetMap))
+	i := 0
+	for asset, _ := range assetMap {
+		assetList[i] = asset
+		i++
+	}
+	sort.Strings(assetList)
+	/*for _, asset := range assetList {
+		fmt.Println(asset)
+	}
+	*/
+}
+
+func getFileAssets(path string, assetMap map[string]int) {
+	fmt.Println("Processing file ", path)
 	xmlStruct := RecordSet{}
-	if err := serzExe.Run(); err != nil {
+	pathXml := strings.ReplaceAll(path, "bin", "xml")
+	//Run serz on file
+	cmd := exec.Command("serz.exe", path)
+	if err := cmd.Run(); err != nil {
 		fmt.Println("Error: ", err)
 	}
-
-	xmlFile, err := os.Open("./files/" + testFile + ".xml")
+	// Open xml file
+	xmlFile, err := os.Open(pathXml)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(xmlFile)
-	info, err := os.Lstat("./files/" + testFile + ".xml")
+	defer xmlFile.Close()
+	info, err := os.Lstat(pathXml)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println("The size of the xml is: ", info.Size())
 	xmlBytes := make([]byte, info.Size())
 	numRead, err := xmlFile.Read(xmlBytes)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println("Number of bytes read: ", numRead)
-
+	// Check that xml processing looks correct
+	if numRead != int(info.Size()) {
+		log.Fatal("Size mismatch on file ", path)
+	}
+	// Unmarshal xml
 	err = xml.Unmarshal(xmlBytes, &xmlStruct)
 	if err != nil {
 		log.Fatal(err)
 	}
-	assetMap := make(map[string]int)
-	entityCount := len(xmlStruct.Records[0].Entities)
+	// Add assets to asset map
+	entityCount := len(xmlStruct.Records.Entities)
 	for i := 0; i < entityCount; i++ {
-		asset := xmlStruct.Records[0].Entities[i].Blueprints[0].BlueprintLib[0].BlueprintID
+		asset := xmlStruct.Records.Entities[i].Blueprints.BlueprintLib.BlueprintID
 		if assetMap[asset] == 0 {
-			fmt.Println(asset)
 			assetMap[asset]++
 		}
 	}
